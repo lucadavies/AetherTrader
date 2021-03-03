dfb856f8d2bf4e1edcc02c98ab44931309cd33a5
Luca Davies <luca-davies@outlook.com>
2021-03-03 23:44:47 +0000

    Add Javadocs

--- BitstampAPIConnection.java
+++ BitstampAPIConnection.java
@@ -12,8 +12,10 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Hex;
 
-// TODO document this class at least a little bit...
-
+/**
+ * Provides a facility to send both private and public API calls to Bitstamp. There is no persistent connection, each 
+ * call a seperate connection, though the <code>HttpClient</code> instance persists.
+ */
 public class BitstampAPIConnection
 {
     private String defaultApiKeyPath = "key";
@@ -24,16 +26,32 @@ public class BitstampAPIConnection
     private final int MAX_RETRY = 3;
     private final HttpClient client = HttpClient.newHttpClient();
 
+    /**
+     * Creates a new BitstampAPIConnection instance, attempting to load keys from default locations relative to the 
+     * execution location: "key" and "secretKey", (No file extension).
+     */
     public BitstampAPIConnection()
     {
         loadKeys(defaultApiKeyPath, defaultApiKeySecretPath);
     }
 
+    /**
+     * Creates a new BitstampAPIConnection instance, attempting to load keys from the provided locations.
+     *  
+     * @param apiKeyPath path to load API Key from
+     * @param apiKeySecretPath path to load the API Key Scret from
+     */
     public BitstampAPIConnection(String apiKeyPath, String apiKeySecretPath)
     {
         loadKeys(apiKeyPath, apiKeySecretPath);
     }
 
+    /**
+     * Loads keys into this instance using the provided paths.
+     * 
+     * @param keyPath path to load API Key from
+     * @param keySecretPath path to load the API Key Scret from
+     */
     private void loadKeys(String keyPath, String keySecretPath)
     {
         try
@@ -49,6 +67,12 @@ public class BitstampAPIConnection
         }
     }
     
+    /**
+     * Send an API call to a public endpoint on Bitstamp's API.
+     * 
+     * @param endPoint the endpoint to call
+     * @return the API endpoint response
+     */
     public String sendPublicRequest(String endPoint)
     {
         String urlHost = "www.bitstamp.net";
@@ -92,6 +116,13 @@ public class BitstampAPIConnection
         
     }
 
+    /**
+     * Send an API call to a public endpoint on Bitstamp's API.
+     * 
+     * @param endPoint the endpoint to call
+     * @param params the parameters to send with the request
+     * @return the API endpoint response
+     */
     public String sendPublicRequest(String endPoint, String[] params)
     {
         String urlHost = "www.bitstamp.net";
@@ -140,6 +171,12 @@ public class BitstampAPIConnection
         }
     }
 
+    /**
+     * Send an API call to a public endpoint on Bitstamp's API.
+     * 
+     * @param endPoint the endpoint to call
+     * @return the API endpoint response
+     */
     public String sendPrivateRequest(String endPoint)
     {
         // Check API Key and API Key Secret are present
@@ -230,6 +267,13 @@ public class BitstampAPIConnection
         }
     }
 
+    /**
+     * Send an API call to a public endpoint on Bitstamp's API.
+     * 
+     * @param endPoint the endpoint to call
+     * @param params the parameters to send with the request
+     * @return the API endpoint response
+     */
     public String sendPrivateRequest(String endPoint, String[] params)
     {
         // Check API Key and API Key Secret are present
