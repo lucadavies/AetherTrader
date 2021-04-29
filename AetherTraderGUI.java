714479c297db65b9105eb0a96bc16511c11a6ad9
Luca Davies <luca-davies@outlook.com>
2021-04-29 11:01:06 +0100

    Add optional Look & Feel set method

--- AetherTraderGUI.java
+++ AetherTraderGUI.java
@@ -215,8 +215,30 @@ public class AetherTraderGUI extends TimerTask implements ActionListener
         lblVolume.setText(data.getBigDecimal("volume").setScale(2, RoundingMode.FLOOR).toString() + "BTC");
     }
 
+    private static void setLookFeel()
+    {
+        try
+        {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+        // handle exception
+        }
+        catch (ClassNotFoundException e) {
+        // handle exception
+        }
+        catch (InstantiationException e) {
+        // handle exception
+        }
+        catch (IllegalAccessException e) {
+        // handle exception
+        }
+    }
+
     public static void main(String[] args)
     {
+        //setLookFeel();
         new AetherTraderGUI();
     }
 }
